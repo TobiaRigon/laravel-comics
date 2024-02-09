4e259c3697a5e7da09361205482d2a4591f4fba9
TobiaRigon <rigontobia@gmail.com>
2024-02-09 17:26:09 +0100

    Feat:Main style

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,22 +1,111 @@
+
 @extends('layouts.main-layout')
+
 @section('head')
     <title>Home</title>
 @endsection
+
 @section('content')
-<h1>Lista dei fumetti</h1>
-<ul>
-    @foreach($comics as $comic)
-        <li>
-            <h2>{{ $comic['title'] }}</h2>
-            <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
-            <p>{{ $comic['description'] }}</p>
-            <p>Price: {{ $comic['price'] }}</p>
-            <p>Series: {{ $comic['series'] }}</p>
-            <p>Sale Date: {{ $comic['sale_date'] }}</p>
-            <p>Type: {{ $comic['type'] }}</p>
-            <p>Artists: {{ implode(', ', $comic['artists']) }}</p>
-            <p>Writers: {{ implode(', ', $comic['writers']) }}</p>
-        </li>
-    @endforeach
-</ul>
+<main>
+<div class="my_jumbo">
+    <img src="/img/jumbotron.jpg" alt="">
+</div>
+
+    <div class="my_container">
+        <div id="current" class="my_badge">Current series</div>
+        <ul class="my_comics-list">
+            @foreach($comics as $comic)
+                <li>
+                    <div class="my_prod_card">
+                        <img src="https://via.placeholder.com/200x240" alt="{{ $comic['title'] }}">
+                        <h3>{{ $comic['series'] }}</h3>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+        <a href="#" id="load" class="my_badge">Load more</a>
+    </div>
+</main>
+
+<style lang="scss">
+    main{
+        background-color: #181818;
+            padding-bottom: 30px;
+
+
+            .my_jumbo {
+            max-height: 300px;
+            overflow: hidden;
+
+        }
+
+        .my_jumbo img {
+            width: 100%;
+            object-fit: cover;
+        }
+
+
+
+        .my_badge {
+            background-color: #007bff;
+            padding: 10px 20px;
+            text-transform: uppercase;
+            font-weight: 600;
+            color: white;
+            text-decoration: none;
+            display: inline-block;
+        }
+
+        ul {
+            list-style-type: none;
+            display: flex;
+            justify-content: center;
+            flex-wrap: wrap;
+            width: 100%;
+            margin: 40px auto;
+            gap: 15px;
+
+            li {
+                flex: 0 0 15%;
+                display: flex;
+                flex-direction: column;
+                margin:20px 0;
+
+
+                .my_prod_card {
+                    display: flex;
+                    flex-direction: column;
+                    height: 100%;
+                }
+
+                img {
+                    width: 200px;
+                    height: 240px;
+                }
+
+                h3 {
+                    padding-top: 15px;
+                    text-decoration: none;
+                    text-transform: uppercase;
+                    font-weight: 600;
+                    color: #a6a6a6;
+                    margin: 0;
+                }
+            }
+        }
+
+        #current {
+            position: relative;
+            transform: translate(-200% , -50%);
+            top: 0;
+            left: 0;
+        }
+
+        .my_container {
+            width: 90%;
+            margin: 0 auto;
+        }
+    }
+
+</style>
 @endsection
