4667aba1d4da390264348bb79c50652e68dec0a9
TobiaRigon <rigontobia@gmail.com>
2024-02-09 15:09:59 +0100

    Config: files set up , rooter setup , index.blade.php setup

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,5 +3,20 @@
     <title>Home</title>
 @endsection
 @section('content')
-    <h1>Hello, World!</h1>
+<h1>Lista dei fumetti</h1>
+<ul>
+    @foreach($comics as $comic)
+        <li>
+            <h2>{{ $comic['title'] }}</h2>
+            <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+            <p>{{ $comic['description'] }}</p>
+            <p>Price: {{ $comic['price'] }}</p>
+            <p>Series: {{ $comic['series'] }}</p>
+            <p>Sale Date: {{ $comic['sale_date'] }}</p>
+            <p>Type: {{ $comic['type'] }}</p>
+            <p>Artists: {{ implode(', ', $comic['artists']) }}</p>
+            <p>Writers: {{ implode(', ', $comic['writers']) }}</p>
+        </li>
+    @endforeach
+</ul>
 @endsection
