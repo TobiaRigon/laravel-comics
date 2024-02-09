96aaa345dc6272c3d4ef6125da009ac268977250
TobiaRigon <rigontobia@gmail.com>
2024-02-09 15:34:13 +0100

    Feat:Header + scss

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,80 @@
 <header>
-    <h1>Hello from Header</h1>
+    <div class="blu_stripe">
+
+    </div>
+    <div class="container">
+        <img src="{{ asset('/dc_logo_icon.png') }}" alt="dc-logo">
+        <div class="menu">
+            <nav>
+                <ul>
+                    @foreach($links as $link)
+                        <li class="{{ $link['current'] ? 'active' : '' }}">
+                            <a href="{{ $link['url'] }}">
+                                {{ $link['text'] }}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </nav>
+        </div>
+    </div>
 </header>
+
+<style scoped lang="scss">
+
+    .blu_stripe{
+        width: 100%;
+        height: 20px;
+        background-color: #0282f9;
+    }
+
+
+
+    .container {
+        background-color: white;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        img {
+            height:100px;
+            padding: 15px 0;
+        }
+
+        .menu {
+            display: inline-block;
+            text-align: end;
+
+            ul {
+                display: flex;
+                gap: 20px;
+                list-style-type: none;
+
+
+
+                li {
+                    padding: 55px 0;
+
+
+
+                    a {
+                        text-decoration: none;
+                        text-transform: uppercase;
+                        font-weight: 600;
+                        color: #707070;
+                    }
+
+                    &:hover {
+                        border-bottom: 3px solid #0282f9;
+                    }
+                }
+
+                .active {
+                    border-bottom: 3px solid #0282f9;
+                }
+
+
+            }
+        }
+    }
+    </style>
--- routes/web.php
+++ routes/web.php
@@ -218,5 +218,61 @@ Route::get('/', function () {
         ],
     ];
 
+    $links = [
+        [
+            "text" => "Home",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "Comics",
+            "url" => "#",
+            "current" => true,
+        ],
+        [
+            "text" => "Movies",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "TV",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "Games",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "Collectibles",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "Video",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "Fans",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "News",
+            "url" => "#",
+            "current" => false,
+        ],
+        [
+            "text" => "Shop",
+            "url" => "#",
+            "current" => false,
+        ],
+    ];
+
+    return view('pages.index', compact('comics', 'links'));
+
+
     return view('pages.index', compact('comics'));
 });
